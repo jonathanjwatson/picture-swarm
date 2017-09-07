e891a9d85a749333f3c9d8058fcc4e634d7e1a3b
jwats287 <jwats287@users.noreply.github.com>
2017-09-07 14:35:11 -0400

    User Sign Up works

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,27 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PictureList from "./components/PictureList";
 import PictureIndividual from "./components/PictureIndividual";
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import GlobalNav from './components/GlobalNav';
+import { setAxiosDefaults } from './util';
 import "./App.css";
 
 class App extends Component {
+  componentWillMount(){
+    setAxiosDefaults();
+  }
+  
   render() {
+  
     return (
       <Router>
         <div className="App">
-          <div>
-            <h1>Picture Swarm</h1>
-
-          </div>
+        <GlobalNav />
           <Route exact path="/" component={PictureList} />
           <Route path="/picture/:id" component={PictureIndividual} />
+          <Route exact path="/signUp" component={SignUp} />
+          <Route exact path="/signIn" component={SignIn} />
         </div>
       </Router>
     );

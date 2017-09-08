60cee33c94a45b992caa6d4d9807be3d6da6dc26
jwats287 <jwats287@users.noreply.github.com>
2017-09-08 09:15:40 -0400

    Styled nav and buttons

--- client/src/components/GlobalNav.jsx
+++ client/src/components/GlobalNav.jsx
@@ -9,7 +9,7 @@ const Nav = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 0 2.5%;
-  background-color: rgba(2, 178, 208, 1);
+  background-color: rgba(0, 51, 82, 1);
   box-shadow: 0px 1px 6px black;
   a {
     text-decoration: none;
@@ -17,6 +17,9 @@ const Nav = styled.div`
     &:visited {
       color: white;
     }
+  }
+  span {
+    color: #3AA9EA;
   }
 `;
 

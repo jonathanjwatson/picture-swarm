28323a7436632031e2e6a68a7e7942d3eeb6df02
jwats287 <jwats287@users.noreply.github.com>
2017-09-12 15:30:46 -0400

    Tag migration and seed update

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
 # before_action :
 
     def index
-        current_user.admin?
+        # current_user.admin?
         @users = User.all
         render json: @users
       end

bfa26328ae7c9d84f9df6ee45e4f0272f6d8b664
jwats287 <jwats287@users.noreply.github.com>
2017-09-12 13:04:55 -0400

    Initial setup for users list

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,9 @@
+class Api::UsersController < ApplicationController
+before_action :
+
+    def index
+        current_user.admin?
+        @users = User.all
+        render json: @users
+      end
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     resources :pictures
+    resources :users
   end
   # api_pictures_search_path returns /api/pictures#search
 end
